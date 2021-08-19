7c7fceafb7bc690a03714369e958dbda80cf6e1e
Nicolas Guzman Rivera <guzman.r.nicolas@gmail.com>
2021-08-18 21:57:31 -0400

    cards

--- src/Home.js
+++ src/Home.js
@@ -1,87 +1,21 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Badge from 'react-bootstrap/Badge';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
 import './css/App.css';
-import { Link } from 'react-router-dom';
+
 
 
 
 const Home = () => {
     return (
-<Container>
-{/*   <nav className="navbar navbar-dark bg-dark">
-    <div className="contaner-fluid">
-  <Link to="/personajes"><button className="btn btn-outline-success text-start mx-3" type="button">  Personajes</button></Link>
-  <Link to="/personajes"><button className="btn btn-outline-success mx-3" type="button">  Vehiculos</button></Link>
-  <Link to="/personajes"><button className="btn btn-outline-success mx-3" type="button">  Planetas</button></Link>
+<div className="container">
+  <div className="row">
+    <div className="h1 col-md-2">Inicio</div>
+    <div className="gif col-md-6">
+      <img src="https://media.giphy.com/media/RLKbCYRD4EtzUVkbMt/giphy.gif"></img>
+      <img src="https://media.giphy.com/media/3o84sCEktZAjG9flte/giphy.gif"></img>
+      <img src="https://media.giphy.com/media/3oeSAFBvE1Tn89ysfK/giphy.gif"></img>
+    </div>
   </div>
-  </nav> */}
-    <Row className="top justify-content-md-center">
-     
-      <Col xl={4} className="mt-5">
-      <Card className="text-center">
-  <Card.Header>Featured</Card.Header>
-  <Card.Body>
-    <Card.Title>Special title treatment</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-  <Card.Footer className="text-muted">2 days ago</Card.Footer>
-</Card>
-</Col>
-
-<Col xl={2}></Col>
-
-<Col xl={4} className="mt-5">
-   <Jumbotron fluid>
-  <Container>
-  <div className="h1 mt-4">Fluid jumbotron</div>
-    <p>
-      This is a modified jumbotron that occupies the entire horizontal space of
-      its parent.
-    </p>
-  </Container>
-</Jumbotron>
-</Col>
-
-<Col xl={4} className="mt-5">
-   <Jumbotron fluid>
-  <Container>
-    <div className="h1 mt-4">Fluid jumbotron</div>
-    <p>
-      This is a modified jumbotron that occupies the entire horizontal space of
-      its parent.
-    </p>
-  </Container>
-</Jumbotron>
-</Col>
-
-<Col xl={2}></Col>
-
-<Col xl={4} className="mt-5">
-   
-   <Card className="text-center">
-  <Card.Header>Featured</Card.Header>
-  <Card.Body>
-    <Card.Title>Special title treatment</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-  <Card.Footer className="text-muted">2 days ago</Card.Footer>
-</Card>
-
-</Col>
-    </Row>
-</Container>
+</div>
        
     )
 }
--- src/views/characters.js
+++ src/views/characters.js
@@ -4,7 +4,54 @@ const Characters = () => {
 
 return (
     <div className="container">
-        <h1>Personajes</h1>
+        <div className="row">
+        <div class="card-group">
+  <div className="card">
+    <img src="..." class="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img src="..." class="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img src="..." class="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+
+<br></br>
+
+<div className="card">
+    <img src="..." class="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card col-md-2">
+    <img src="..." class="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+
+</div>
+        </div>
     </div>
 )
 
